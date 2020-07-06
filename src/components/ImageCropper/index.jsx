57657d45cea498e26eff23ca24d0cba55636b042
huzhiwu1 <1627983849@qq.com>
2020-07-06 18:59:48 +0800

    给图片添加触摸事件

--- src/components/ImageCropper/index.jsx
+++ src/components/ImageCropper/index.jsx
@@ -1,9 +1,9 @@
-import Taro, { Component } from '@tarojs/taro';
-import { View, Canvas } from '@tarojs/components';
-import './index.scss';
+import Taro, { Component } from "@tarojs/taro";
+import { View, Canvas } from "@tarojs/components";
+import "./index.scss";
 export default class ImageCropper extends Component {
 	static defaultProps = {
-		imgSrc: '', //图片路径
+		imgSrc: "", //图片路径
 		cut_ratio: 0.5, //裁剪框的 宽/高 比
 	};
 	constructor(props) {
@@ -39,14 +39,14 @@ export default class ImageCropper extends Component {
 	 *  获取canvas上下文
 	 */
 	initCanvas() {
-		this.ctx = Taro.createCanvasContext('canvas', this);
+		this.ctx = Taro.createCanvasContext("canvas", this);
 	}
 	/**
 	 * 获取设备屏幕的宽高
 	 */
 	async getDeviceInfo() {
 		const { windowHeight, windowWidth } = await Taro.getSystemInfoSync();
-		return new Promise(resolve => {
+		return new Promise((resolve) => {
 			this.setState(
 				{
 					_window_height: windowHeight,
@@ -64,7 +64,7 @@ export default class ImageCropper extends Component {
 		const { width, height, path } = await Taro.getImageInfo({
 			src: imgSrc,
 		});
-		return new Promise(resolve => {
+		return new Promise((resolve) => {
 			this.setState(
 				{
 					imgSrc,
@@ -91,7 +91,7 @@ export default class ImageCropper extends Component {
 			initial_cut_height = Math.floor(_window_height / 2);
 			initial_cut_width = initial_cut_height * cut_ratio;
 		}
-		return new Promise(resolve => {
+		return new Promise((resolve) => {
 			this.setState(
 				{
 					_cut_height: initial_cut_height,
@@ -113,7 +113,7 @@ export default class ImageCropper extends Component {
 		} = this.state;
 		const _cut_top = (_window_height - _cut_height) / 2; //因为裁剪框居中，所以可直接对半分
 		const _cut_left = (_window_width - _cut_width) / 2;
-		return new Promise(resolve => {
+		return new Promise((resolve) => {
 			this.setState(
 				{
 					_cut_top,
@@ -139,7 +139,7 @@ export default class ImageCropper extends Component {
 			_img_height = _cut_height;
 			_img_width = _img_height * _img_ratio;
 		}
-		return new Promise(resovle => {
+		return new Promise((resovle) => {
 			this.setState(
 				{
 					_img_height,
@@ -163,7 +163,7 @@ export default class ImageCropper extends Component {
 		let _img_left, _img_top;
 		_img_left = (_window_width - _img_width) / 2;
 		_img_top = (_window_height - _img_height) / 2;
-		return new Promise(resolve => {
+		return new Promise((resolve) => {
 			this.setState(
 				{
 					_img_left,
@@ -172,6 +172,13 @@ export default class ImageCropper extends Component {
 				resolve
 			);
 		});
+	}
+
+	/**
+	 *  图片的点击，移动，移动结束事件
+	 */
+	_img_touch_start(e) {
+		console.log(e, "e");
 	}
 	render() {
 		const {
@@ -186,39 +193,45 @@ export default class ImageCropper extends Component {
 			_window_width,
 		} = this.state;
 		return (
-			<View className='image-cropper-wrapper'>
-				<View className='bg_container'>
-					<View className='bg_top'></View>
-					<View className='bg_middle'>
-						<View className='bg_middle_left'></View>
+			<View className="image-cropper-wrapper">
+				<View className="bg_container">
+					<View className="bg_top"></View>
+					<View className="bg_middle">
+						<View className="bg_middle_left"></View>
 						<View
-							className='cut_wrapper'
-							style={{ width: _cut_width + 'px', height: _cut_height + 'px' }}
+							className="cut_wrapper"
+							style={{
+								width: _cut_width + "px",
+								height: _cut_height + "px",
+							}}
 						>
-							<View className='border border-top-left'></View>
-							<View className='border border-top-right'></View>
-							<View className='border border-right-top'></View>
-							<View className='border border-bottom-right'></View>
-							<View className='border border-right-bottom'></View>
-							<View className='border border-bottom-left'></View>
-							<View className='border border-left-bottom'></View>
-							<View className='border border-left-top'></View>
+							<View className="border border-top-left"></View>
+							<View className="border border-top-right"></View>
+							<View className="border border-right-top"></View>
+							<View className="border border-bottom-right"></View>
+							<View className="border border-right-bottom"></View>
+							<View className="border border-bottom-left"></View>
+							<View className="border border-left-bottom"></View>
+							<View className="border border-left-top"></View>
 						</View>
-						<View className='bg_middle_right'></View>
+						<View className="bg_middle_right"></View>
 					</View>
-					<View className='bg_bottom'></View>
+					<View className="bg_bottom"></View>
 				</View>
 				<Image
-					className='img'
+					className="img"
 					src={imgSrc}
 					style={{
-						width: _img_width + 'px',
-						height: _img_height + 'px',
-						top: _img_top + 'px',
-						left: _img_left + 'px',
+						width: _img_width + "px",
+						height: _img_height + "px",
+						top: _img_top + "px",
+						left: _img_left + "px",
 					}}
+					onTouchStart={this._img_touch_start}
+					onTouchMove={this._img_touch_move}
+					onTouchEnd={this._img_touch_end}
 				/>
-				<Canvas canvasId='canvas' disableScroll={false}></Canvas>
+				<Canvas canvasId="canvas" disableScroll={false}></Canvas>
 			</View>
 		);
 	}
